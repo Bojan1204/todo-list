5f161e256c02f8cb239fdfa65ea80a576f399054
Bojan Bosnic <56975621+Bojan1204@users.noreply.github.com>
2022-01-10 02:36:02 +0100

    initial commit

---
+++ src/TodoApp.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+
+const TodoApp = (props) => {
+  return (
+    <Paper
+      style={{ padding: 0, margin: 0, height: "100vh", backgroundColor: "red" }}
+      elevation={0}
+    >
+      <AppBar color='primary' position="static" style={{ height:"64px"}}>
+          <Toolbar>
+              <Typography color="inherit">TODOS WITH HOOKS</Typography>
+          </Toolbar>
+      </AppBar>
+    </Paper>
+  );
+};
+
+export default TodoApp;
