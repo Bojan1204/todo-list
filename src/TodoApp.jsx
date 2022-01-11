661d6b51089dd06d91683f43b8bf0a9c6580e6d2
Bojan Bosnic <56975621+Bojan1204@users.noreply.github.com>
2022-01-11 01:22:29 +0100

    add form field and set todo list

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
+import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
+
 
 const TodoApp = (props) => {
+  const initialTodos = [
+    { id: 1, task: "Clean Fishtank", completed: false },
+    { id: 2, task: "Wash Car", completed: true },
+    { id: 3, task: "Grow Beard", completed: false },
+  ];
+
+  const [todos, setTodos] = useState(initialTodos);
+  const addTodo = (newTodoText) => {
+    setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
+  };
+
   return (
     <Paper
-      style={{ padding: 0, margin: 0, height: "100vh", backgroundColor: "red" }}
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "white",
+      }}
       elevation={0}
     >
-      <AppBar color='primary' position="static" style={{ height:"64px"}}>
-          <Toolbar>
-              <Typography color="inherit">TODOS WITH HOOKS</Typography>
-          </Toolbar>
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS WITH HOOKS</Typography>
+        </Toolbar>
       </AppBar>
+      <Grid container justify="center" style={{marginTop: "1rem"}}>
+        <Grid item xs={11} md={8} lg={4}> 
+        <TodoForm addTodo={addTodo} />
+        <TodoList todos={todos} />
+        </Grid>
+      </Grid>
     </Paper>
   );
 };
---
+++ src/TodoList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItem";
+
+const TodoList = (props) => {
+  return (
+    <Paper>
+        <List> 
+      {props.todos.map((todo) => (
+          <> 
+        <ListItemText>{todo.task}</ListItemText>
+        <Divider/>
+        </>
+      ))}
+      </List>
+    </Paper>
+  );
+};
+
+export default TodoList;
